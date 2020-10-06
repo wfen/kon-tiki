ec0163dbb621a323f0239fddfca0097825171c7b
Ward Fenton <ward.fenton@oath.com>
2020-10-06 13:18:19 -0400

    01-bootstrapping: Node Initialization

--- raft.py
+++ raft.py
@@ -4,6 +4,21 @@ from __future__ import unicode_literals
 import sys
 import json
 from pprint import pformat
+import datetime
+import time
+
+# Utilities
+
+
+def log(*args):
+    """Helper function for logging stuff to stderr"""
+    first = True
+    sys.stderr.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f "))
+    for i in range(len(args)):
+        sys.stderr.write(str(args[i]))
+        if i < (len(args) - 1):
+            sys.stderr.write(" ")
+    sys.stderr.write("\n")
 
 
 class Net:
@@ -20,7 +35,31 @@ class Net:
             return None
 
         msg = json.loads(line)
-        sys.stderr.write("Received\n" + pformat(msg) + "\n")
+        return msg
 
 
-Net().process_msg()
+class RaftNode:
+    def __init__(self):
+        self.node_id = None  # Our node ID
+        self.node_ids = None  # The set of node IDs
+
+        self.state = "nascent"  # One of nascent, follower, candidate, or leader
+
+        self.net = Net()
+
+    def main(self):
+        """Entry point"""
+        log("Online.")
+
+        # Handle initialization message
+        msg = self.net.process_msg()
+        body = msg["body"]
+        if body["type"] != "raft_init":
+            raise RuntimeError("Unexpected message " + pformat(msg))
+
+        self.node_id = body["node_id"]
+        self.node_ids = body["node_ids"]
+        log("I am:", self.node_id)
+
+
+RaftNode().main()
