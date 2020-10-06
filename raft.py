ee80f4e2f46ff21b7abe9a4a414023e63b899258
Ward Fenton <ward.fenton@oath.com>
2020-10-06 12:51:05 -0400

    01-bootstrapping: A Tiny Network Server

--- raft.py
+++ raft.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python
 
+from __future__ import unicode_literals
 import sys
-sys.stderr.write('hello, world\n')
+import json
+from pprint import pformat
+
+
+class Net:
+    """Handles console IO for sending and receiving messages."""
+
+    def __init__(self):
+        """Constructs a new network client."""
+        pass
+
+    def process_msg(self):
+        """Handles a message from stdin, if one is currently available."""
+        line = sys.stdin.readline()
+        if not line:
+            return None
+
+        msg = json.loads(line)
+        sys.stderr.write("Received\n" + pformat(msg) + "\n")
+
+
+Net().process_msg()
